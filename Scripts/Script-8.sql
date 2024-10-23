77f35903fe553cf594199db9d8f8d6b151488113
Kitae's Github <aqua0405@gmail.com>
2024-10-23 19:13:42 +0900

    Create Script-8.sql

---
+++ Scripts/Script-8.sql
@@ -0,0 +1,3 @@
+INSERT INTO kt_fastapi.camera
+(camera_name, user_id, model_id, camera_url, graph_url, stream_url, count, label)
+VALUES('ScooterCam', 1, 2, 'http://test:8000', 'http://test:8000/api/scooter/graph/2/2', 'http://test:8000/api/scooter/stream/2/2', 1, 1);
