d954e20dcde359c76f86b1a1df15c8ba9db06a49
Kitae's Github <aqua0405@gmail.com>
2024-10-23 18:21:22 +0900

    Update OneToMay.sql

--- OneToMay.sql
+++ OneToMay.sql
@@ -14,3 +14,15 @@ SELECT  *
 FROM customers c , orders o ; 
 
 -- 내부 조인
+SELECT * 
+FROM customers c JOIN orders o 
+ON o.customer_id = c.id;
+
+SELECT * 
+FROM orders o JOIN customers c
+ON c.id = o.customer_id;
+
+SELECT first_name, last_name, order_date, amount 
+FROM customers c JOIN orders o 
+ON o.customer_id = c.id;
+
