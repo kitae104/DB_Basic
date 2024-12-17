d35d2f06087fdc5cdfa11828f4405c19a7e1dee1
Kitae's Github <aqua0405@gmail.com>
2024-12-17 22:53:43 +0900

    Create Script-9.sql

---
+++ Scripts/Script-9.sql
@@ -0,0 +1,24 @@
+INSERT INTO yulo_db.camera
+(camera_seq, building_seq, model_seq, camera_url, reg_date, update_date, camera_name)
+VALUES(1, 7, 5, 'rtsp://tester:tester@192.168.35.179/stream1', now(), now(), '사람 카메라');
+
+INSERT INTO yulo_db.model
+(model_seq, reg_date, update_date, model_name)
+VALUES(2, now(), now(), 'helmet');
+
+INSERT INTO yulo_db.model
+(model_seq, reg_date, update_date, model_name)
+VALUES(3, now(), now(), 'Scooter');
+
+INSERT INTO yulo_db.model
+(model_seq, reg_date, update_date, model_name)
+VALUES(4, now(), now(), 'fire');
+
+INSERT INTO yulo_db.model
+(model_seq, reg_date, update_date, model_name)
+VALUES(5, now(), now(), 'person');
+
+INSERT INTO yulo_db.model
+(model_seq, reg_date, update_date, model_name)
+VALUES(6, now(), now(), 'tv');
+
