b8799053aec688b057880976ade0d5bade305775
Kitae's Github <aqua0405@gmail.com>
2025-01-22 22:33:00 +0900

    Script-10

---
+++ Scripts/Script-10.sql
@@ -0,0 +1,1 @@
+INSERT INTO nodejs_basic.products (id, title, price, description, imageUrl) VALUES(1, '명품 JAVA Programming', 330000, '자바(Java)는 그 이전 시대에 있었던 프로그래밍 언어에서 한 차원 진화된 개념으로 개발된 가히 혁명적 언어이며 플랫폼입니다.', 'https://www.booksr.co.kr/wp-content/uploads/2022/01/bookcover_img130-300x411.jpg');
