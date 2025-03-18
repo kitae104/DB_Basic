d03ddaa9f6d211a50f2b34fd7b666d9892f32db5
Kitae's Github <aqua0405@gmail.com>
2025-03-18 14:35:08 +0900

    Update Instagram_crud.sql

--- Instagram/Instagram_crud.sql
+++ Instagram/Instagram_crud.sql
@@ -43,3 +43,79 @@ INSERT INTO comments (comment_text, user_id, photo_id) VALUES
 ('Nice pic12!', 1, 12);
 
 SELECT * FROM comments;
+
+INSERT INTO likes (user_id, photo_id) VALUES
+(2, 1),
+(2, 2),
+(3, 3),
+(3, 4),
+(4, 5),
+(4, 6),
+(5, 7),
+(5, 8),
+(6, 9),
+(6, 10),
+(1, 11),
+(1, 12);
+
+SELECT * FROM likes;
+
+INSERT INTO follows (follower_id, followee_id) VALUES
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6),
+(2, 1),
+(2, 3),
+(2, 4),
+(2, 5),
+(2, 6),
+(3, 1),
+(3, 2),
+(3, 4),
+(3, 5),
+(3, 6),
+(4, 1),
+(4, 2),
+(4, 3),
+(4, 5),
+(4, 6),
+(5, 1),
+(5, 2),
+(5, 3),
+(5, 4),
+(5, 6),
+(6, 1),
+(6, 2),
+(6, 3),
+(6, 4),
+(6, 5);
+
+SELECT * FROM follows;  
+
+INSERT INTO tags (tag_name) VALUES
+('food'),
+('travel'),
+('nature'),
+('fashion'),
+('music'),
+('sports'); 
+
+SELECT * FROM tags;
+
+INSERT INTO photo_tags (photo_id, tag_id) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 1),
+(8, 2),
+(9, 3),
+(10, 4),
+(11, 5),
+(12, 6);
+
+SELECT * FROM photo_tags;
