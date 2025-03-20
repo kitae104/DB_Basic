290af76217601f1c87082762deb2dadc6a4667a4
Kitae's Github <aqua0405@gmail.com>
2025-03-20 11:06:25 +0900

    Create Script-11.sql

---
+++ Scripts/Script-11.sql
@@ -0,0 +1,3 @@
+INSERT INTO todo_list.user_auth
+(reg_time, update_time, user_id, auth, username, created_by, modified_by)
+VALUES('2025-03-10 14:42:36.899521', '2025-03-10 14:42:36.899521', 2, 'ROLE_ADMIN', 'admin', 'anonymousUser', 'anonymousUser');
